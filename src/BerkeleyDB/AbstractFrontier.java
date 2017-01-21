7e356353e9f7ce7c44cbf707611c1820e57c65ec
TechStone <TStone@TechStoneMac.local>
2017-01-21 15:41:15 +0800

    1-21

---
+++ src/BerkeleyDB/AbstractFrontier.java
@@ -0,0 +1,7 @@
+package BerkeleyDB;
+
+/**
+ * Created by TStone on 1/19/17.
+ */
+public class AbstractFrontier {
+}
---
+++ src/BerkeleyDB/Frontier.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by TStone on 1/19/17.
+ */
+public class BerkeleyDB {
+}
